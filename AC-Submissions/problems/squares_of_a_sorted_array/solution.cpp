22ffb0254fc30c2c70dafada27b0498441042647
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-06-20 14:09:09 +0000

    Sync LeetCode submission - Squares of a Sorted Array (cpp)

--- AC-Submissions/problems/squares_of_a_sorted_array/solution.cpp
+++ AC-Submissions/problems/squares_of_a_sorted_array/solution.cpp
@@ -1,35 +1,40 @@
-#include<bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int>v;
         int len=nums.size();
-        int idx=len;
-        for(int i=0;i<len;i++){
-            if(nums[i]>=0){
-                idx=i;
-                break;
-            }
-        }
-        for(int i=idx,j=idx-1;(i<len)||(j>=0);){
-            if(i>=len){
-                v.push_back(nums[j]*nums[j]);
-                j--;
-            }else if(j<0){
-                v.push_back(nums[i]*nums[i]);
-                i++;
-            }else{
-                if(abs(nums[i])<=abs(nums[j])){
-                    v.push_back(nums[i]*nums[i]);
-                    i++;
-                }else{
-                    v.push_back(nums[j]*nums[j]);
-                    j--;
+        int idx;
+        if(nums[len-1]<0){
+            idx=len;
+        }else{
+            for(int i=0;i<len;i++){
+                if(nums[i]>=0){
+                    idx=i;
+                    break;
                 }
             }
         }
-        return v;
+        //cout<<idx<<endl;
+        vector<int>arr;
+        int i=idx-1,j=idx;
+        for(i=idx-1,j=idx;i>=0&&j<len;){
+            if(abs(nums[i])<abs(nums[j])){
+                arr.push_back((nums[i]*nums[i]));
+                i--;
+            }else{
+                arr.push_back((nums[j]*nums[j]));
+                j++;
+            }
+        }
+        
+        while(i>=0){
+            arr.push_back((nums[i]*nums[i]));
+            i--;
+        }
+        while(j<len){
+            arr.push_back((nums[j]*nums[j]));
+            j++;
+        }
+        
+        return arr;
     }
 };
