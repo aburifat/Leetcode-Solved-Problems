47fc125684daf3868b59a1a39e75650082489ef7
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-01 21:37:30 +0000

    [LeetCode Sync] Runtime - 18 ms (6.54%), Memory - 18.4 MB (100.00%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int k=0;
-        int val;
-        if(nums.size()>0){
-            val=nums[0];
-            k=1;
-        }
+        int idx=1;
         for(int i=1;i<nums.size();i++){
-            if(nums[i]!=val){
-                val=nums[i];
-                nums[k]=nums[i];
-                k++;
+            if(nums[i-1]<nums[i]){
+                nums[idx]=nums[i];
+                idx++;
             }
         }
-        return k;
+        return idx;
     }
 };
