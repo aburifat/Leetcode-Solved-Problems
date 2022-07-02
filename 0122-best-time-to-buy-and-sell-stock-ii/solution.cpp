b2acc59dcb8899e9ac3a09f6d6498591a3bcdc49
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-02 11:58:53 +0000

    [LeetCode Sync] Runtime - 28 ms (5.05%), Memory - 16.2 MB (100.00%)

--- 0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -1,35 +1,31 @@
 class Solution {
-    int dp[100000][2];
-    vector<int>arr;
 public:
-    void init(){
-        for(int i=0;i<100000;i++){
-            for(int j=0;j<2;j++)dp[i][j]=-1;
-        }
-    }
-    int ctDp(int buyPrice,int bought,int nowIdx,int mxIdx){
+    int dp[30010][2];
+    int ctDp(int nowIdx, bool bought, int buyPrice,int mxIdx,vector<int>& prices){
         if(dp[nowIdx][bought]!=-1)return dp[nowIdx][bought];
         if(nowIdx==mxIdx){
-            if(bought) return dp[nowIdx][bought]=max(0,arr[nowIdx]-buyPrice);
-            else return dp[nowIdx][bought]=0;
-        }
-        if(bought){
-            int sell_price=max(0,arr[nowIdx]-buyPrice);
-            int val1=sell_price+ctDp(arr[nowIdx],true,nowIdx+1,mxIdx);
-            int val2=ctDp(buyPrice,bought,nowIdx+1,mxIdx);
-            dp[nowIdx][bought]=max(val1,val2);
+            if(bought)dp[nowIdx][bought]=max(0,prices[nowIdx]-buyPrice);
+            else dp[nowIdx][bought]=0;
         }else{
-            int val1=ctDp(0,false,nowIdx+1,mxIdx);
-            int val2=ctDp(arr[nowIdx],true,nowIdx+1,mxIdx);
-            dp[nowIdx][bought]=max(val1,val2);
+            if(bought){
+                int sellPrice=prices[nowIdx]-buyPrice;
+                int val2=max(0,sellPrice+ctDp(nowIdx+1,true,prices[nowIdx],mxIdx,prices));
+                int val1=max(0,ctDp(nowIdx+1,true,buyPrice,mxIdx,prices));
+                dp[nowIdx][bought]=max(val1,val2);
+            }else{
+                int val1=max(0,ctDp(nowIdx+1,false,0,mxIdx,prices));
+                int val2=max(0,ctDp(nowIdx+1,true,prices[nowIdx],mxIdx,prices));
+                dp[nowIdx][bought]=max(val1,val2);
+            }
         }
         return dp[nowIdx][bought];
     }
     int maxProfit(vector<int>& prices) {
-        init();
-        arr=prices;
         int len=prices.size();
-        int ans=ctDp(0,false,0,len-1);
+        for(int i=0;i<30010;i++){
+            for(int j=0;j<2;j++)dp[i][j]=-1;
+        }
+        int ans=ctDp(0,false,0,len-1,prices);
         return ans;
     }
 };
