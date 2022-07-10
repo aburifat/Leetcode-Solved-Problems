4ff70a379bc9aff93a483613756af280dcef96b1
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-10 11:35:31 +0000

    [LeetCode Sync] Runtime - 52 ms (2.64%), Memory - 26.4 MB (100.00%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -3,9 +3,9 @@ public:
     void rotate(vector<int>& nums, int k) {
         int len=nums.size();
         vector<int>v;
-        k=k%len;
+        k%=len;
         for(int i=len-k;i<len;i++)v.push_back(nums[i]);
-        for(int i=0;i<(len-k);i++)v.push_back(nums[i]);
+        for(int i=0;i<len-k;i++)v.push_back(nums[i]);
         nums=v;
     }
 };
