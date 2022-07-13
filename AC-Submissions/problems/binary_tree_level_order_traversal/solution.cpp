83948510b3dc735566c192dcc6c81058749a5495
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-13 17:26:56 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (cpp)

--- AC-Submissions/problems/binary_tree_level_order_traversal/solution.cpp
+++ AC-Submissions/problems/binary_tree_level_order_traversal/solution.cpp
@@ -1,22 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+void dfs(vector<vector<int>> &v, TreeNode* node, int level){
+    vector<int>emp;
+    while(v.size()<=level)v.push_back(emp);
+    v[level].push_back(node->val);
+    TreeNode* left=node->left;
+    TreeNode* right=node->right;
+    if(left!=nullptr)dfs(v,left,level+1);
+    if(right!=nullptr)dfs(v,right,level+1);
+}
+
 class Solution {
-    int MX=2010;
-    vector<vector<int>>tmp,ans;
 public:
-    void insertByLevel(int level, TreeNode* node){
-        if(node){
-            tmp[level].push_back(node->val);
-            insertByLevel(level+1,node->left);
-            insertByLevel(level+1,node->right);
-        }
-    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        tmp.assign(MX,vector<int>());
-        ans.clear();
-        insertByLevel(0,root);
-        for(int i=0;i<2010;i++){
-            if((int)tmp[i].size()==0)break;
-            ans.push_back(tmp[i]);
-        }
+        vector<vector<int>>ans;
+        if(root!=nullptr)dfs(ans,root,0);
         return ans;
     }
 };
