fa7bdc6957e8e95e4dc61d4805f125b938d13579
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-15 20:38:33 +0000

    [LeetCode Sync] Runtime - 30 ms (8.98%), Memory - 19.3 MB (100.00%)

--- 0283-move-zeroes/solution.cpp
+++ 0283-move-zeroes/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>v;
-        int len=nums.size();
-        for(int i=0;i<len;i++){
-            if(nums[i]!=0)v.push_back(nums[i]);
+        int ct=0;
+        for(int i=0,j=0;i<nums.size()&&j<nums.size();){
+            if(nums[i]!=0&&nums[j]!=0)i++,j++;
+            else if(nums[i]!=0)i++;
+            else if(nums[j]==0)j++,ct++;
+            else nums[i]=nums[j],nums[j]=0,i++,j++;
         }
-        int dd=v.size();
-        for(int i=dd;i<len;i++){
-            v.push_back(0);
+        for(int i=nums.size()-1;ct;i--,ct--){
+            nums[i]=0;
         }
-        nums=v;
     }
 };
