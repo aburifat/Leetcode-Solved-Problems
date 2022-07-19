99a8ff175f400906e341b9c8ac6ff21da7549aaa
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-19 20:48:40 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (cpp)

--- AC-Submissions/problems/merge_two_sorted_lists/solution.cpp
+++ AC-Submissions/problems/merge_two_sorted_lists/solution.cpp
@@ -11,26 +11,30 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL)return list2;
-        if(list2==NULL)return list1;
-        ListNode* ans = list1;
-        if(list1->val>list2->val){
-            ans=list2;
+        ListNode* head=nullptr;
+        if(list1==nullptr)return list2;
+        if(list2==nullptr)return list1;
+        if(list1->val<list2->val){
+            head=list1;
+            list1=list1->next;
+        }else{
+            head=list2;
             list2=list2->next;
-        }else list1=list1->next;
-        ListNode* ptr=ans;
-        while(list1&&list2){
+        }
+        ListNode* curr=head;
+        while(list1!=nullptr&&list2!=nullptr){
             if(list1->val<list2->val){
-                ptr->next=list1;
+                curr->next=list1;
+                curr=curr->next;
                 list1=list1->next;
             }else{
-                ptr->next=list2;
+                curr->next=list2;
+                curr=curr->next;
                 list2=list2->next;
             }
-            ptr=ptr->next;
         }
-        if(list1)ptr->next=list1;
-        else if(list2)ptr->next=list2;
-        return ans;
+        if(list1==nullptr)curr->next=list2;
+        else if(list2==nullptr)curr->next=list1;
+        return head;
     }
 };
