9227474e7a297334c6a437f15e8590b1b6e389d8
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-28 20:38:22 +0000

    Sync LeetCode submission - Find the Index of the First Occurrence in a String (cpp)

--- AC-Submissions/problems/find_the_index_of_the_first_occurrence_in_a_string/solution.cpp
+++ AC-Submissions/problems/find_the_index_of_the_first_occurrence_in_a_string/solution.cpp
@@ -1,20 +1,29 @@
 class Solution {
+    vector<int>prefFunc(string s){
+        int len=s.size();
+        vector<int>pi(len);
+        pi[0]=0;
+        for (int i=1;i<len;i++) {
+            int j=pi[i-1];
+            while(j>0&&s[i]!=s[j])
+                j=pi[j-1];
+            if(s[i]==s[j])
+                j++;
+            pi[i]=j;
+        }
+        return pi;
+    }
 public:
     int strStr(string haystack, string needle) {
-        int lenHay=haystack.size();
-        int lenNed=needle.size();
+        int n_len=needle.size();
+        needle=needle+"#"+haystack;
+        vector<int>pi=prefFunc(needle);
+        int h_len=needle.size();
         int ans=-1;
-        for(int i=0;i<lenHay;i++){
-            if(haystack[i]==needle[0]){
-                int ct=0;
-                for(int j=0;j<lenNed;j++){
-                    if(haystack[i+j]==needle[j])ct++;
-                    else break;
-                }
-                if(ct==lenNed){
-                    ans=i;
-                    break;
-                }
+        for(int i=n_len+1;i<h_len;i++){
+            if(pi[i]==n_len){
+                ans=i-(2*n_len);
+                break;
             }
         }
         return ans;
