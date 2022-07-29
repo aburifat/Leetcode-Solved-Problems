754964928315af718bb2190a4646e2bb4c4d624f
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-29 21:58:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 5.8 MB (100.00%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,18 +1,11 @@
 class Solution {
-    int dp[50];
 public:
-    void init(){
-        for(int i=0;i<50;i++)dp[i]=-1;
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
-    }
-    int goBabe(int n){
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]=goBabe(n-1)+goBabe(n-2);
-    }
     int climbStairs(int n) {
-        init();
-        return goBabe(n);
+        int dp[n+5];
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
