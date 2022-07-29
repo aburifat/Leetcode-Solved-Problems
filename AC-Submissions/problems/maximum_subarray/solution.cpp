65201ed5cc597eb9da94f2f12038a69e2a395ba0
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-29 23:38:24 +0000

    Sync LeetCode submission - Maximum Subarray (cpp)

--- AC-Submissions/problems/maximum_subarray/solution.cpp
+++ AC-Submissions/problems/maximum_subarray/solution.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
+    int divide(vector<int>& nums, int l, int r){
+        if(l>r)return INT_MIN;
+        if(l==r)return nums[l];
+        int mid=l+(r-l)/2;
+        int left,right,sum;
+        left=right=sum=nums[mid];
+        for(int i=mid-1;i>=l;i--){
+            sum+=nums[i];
+            left=max(left,sum);
+        }
+        sum=nums[mid];
+        for(int i=mid+1;i<=r;i++){
+            sum+=nums[i];
+            right=max(right,sum);
+        }
+        int subSum=max(left+right-nums[mid],max(left,right));
+        return max(subSum,max(divide(nums,l,mid-1),divide(nums,mid+1,r)));
+    }
     int maxSubArray(vector<int>& nums) {
         int len=nums.size();
-        int sum=nums[0];
-        int tmp=0;
-        for(int i=0;i<len;i++){
-            tmp+=nums[i];
-            if(tmp<0){
-                tmp=0;
-                sum=max(sum,nums[i]);
-                continue;
-            }
-            sum=max(sum,tmp);
-        }
-        return sum;
+        int ans=divide(nums,0,len-1);
+        return ans;
     }
 };
