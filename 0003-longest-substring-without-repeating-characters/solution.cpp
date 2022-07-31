dfc5c0754407dc59e3c5f322446f73303f2d9cb7
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-07-31 12:24:29 +0000

    [LeetCode Sync] Runtime - 51 ms (11.10%), Memory - 8.6 MB (100.00%)

--- 0003-longest-substring-without-repeating-characters/solution.cpp
+++ 0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int len=s.size();
-        map<int,int>mp;
-        int ct=0;
-        int ans=0;
-        for(int i=0,j=0;i<len&&j<len;j++){
-            if(!mp[s[j]]){
-                ct++;
-                mp[s[j]]++;
-            }else{
-                ans=max(ans,ct);
-                while(i<j){
-                    if(s[i]==s[j]){
-                        i++;
-                        break;
-                    }else{
-                        ct--;
-                        mp[s[i]]--;
-                        i++;
-                    }
+        int mx=0,l=0,ct=0;
+        map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            ct++;
+            if(mp[s[i]]>1){
+                mx=max(mx,ct-1);
+                while(mp[s[i]]>1){
+                    mp[s[l]]--;
+                    ct--;
+                    l++;
                 }
             }
-            if(j==len-1)ans=max(ans,ct);
         }
-        return ans;
+        mx=max(mx,ct);
+        return mx;
     }
 };
