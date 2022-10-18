e9095b7de1ddc616d283b71179197469d9e9c905
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-10-18 12:45:01 +0000

    [LeetCode Sync] Runtime - 77 ms (5.49%), Memory - 36 MB (99.88%)

--- 0653-two-sum-iv---input-is-a-bst/solution.cpp
+++ 0653-two-sum-iv---input-is-a-bst/solution.cpp
@@ -1,31 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+bool findSecondValue(TreeNode* brother, TreeNode* curr, int k){
+    if(curr==nullptr)return false;
+    if(curr->val==k&&curr!=brother)return true;
+    else if((curr->val)<k)return findSecondValue(brother,curr->right,k);
+    else return findSecondValue(brother,curr->left,k);
+    return false;
+}
+bool solve(TreeNode* root, TreeNode* curr, int k){
+    if(curr==nullptr)return false;
+    int dif=k-(curr->val);
+    bool ck=findSecondValue(curr,root, dif);
+    if(ck)return true;
+    return solve(root,curr->left,k)|solve(root,curr->right,k);
+}
+ 
 class Solution {
-    bool findVal(TreeNode* brother, TreeNode* curr, int k){
-        if(curr==nullptr)return false;
-        if(curr->val==k&&curr!=brother)return true;
-        else if(curr->val<k)return findVal(brother,curr->right,k);
-        else return findVal(brother,curr->left,k);
-        return false;
-    }
-    bool goBabe(TreeNode* root, TreeNode* curr, int k){
-        if(curr==nullptr)return false;
-        int dif=k-(curr->val);
-        bool ck=findVal(curr,root, dif);
-        if(ck)return true;
-        return goBabe(root,curr->left,k)|goBabe(root,curr->right,k);
-    }
 public:
     bool findTarget(TreeNode* root, int k) {
-        return goBabe(root,root,k);
+        return solve(root,root,k);
     }
 };
