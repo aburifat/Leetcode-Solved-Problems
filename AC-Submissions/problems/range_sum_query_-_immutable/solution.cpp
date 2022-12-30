03c89664a2dd6ce1bf8b2a7c33075cb2e5a9bb68
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2022-12-30 17:02:38 +0000

    Sync LeetCode submission - Range Sum Query - Immutable (cpp)

--- AC-Submissions/problems/range_sum_query_-_immutable/solution.cpp
+++ AC-Submissions/problems/range_sum_query_-_immutable/solution.cpp
@@ -1,52 +1,61 @@
+typedef long long ll;
+static const ll MXN=5e5;
+static const ll K=20;
+ll n=1e5,k;
+ll lg[MXN+5];
+ll arr[MXN+5];
+ll st[K+5][MXN+5];
+
+void setLog(){
+    lg[0]=lg[1]=0;
+    for(ll i=2;i<=MXN;i++)lg[i]=lg[i/2]+1;
+}
+
+ll func(ll a, ll b){
+    ll val=a+b;
+    return val;
+}
+
+void build(){
+    setLog();
+    k=lg[n];
+    k++;
+    for(ll i=0;i<n;i++)st[0][i]=arr[i];
+    for(ll i=1;i<=k;i++){
+        for(ll j=0;j+(1<<i)<=n;j++){
+            st[i][j]=func(st[i-1][j],st[i-1][j+(1<<(i-1))]);
+        }
+    }
+}
+
+ll query(ll l, ll r){
+    ll ans=0;
+    for(ll i=k;i>=0;i--){
+        if((1<<i)<=r-l+1){
+            ans=func(ans,st[i][l]);
+            l+=(1<<i);
+        }
+    }
+    return ans;
+}
+
 class NumArray {
-    vector<int>tree;
-	int Trush=0;
-    int siz;
 public:
-    int combine(int a, int b){
-		return a+b;
-	}
-    void TreeBuild(vector<int>& nums, int idx, int tl, int tr){
-		if(tl==tr)tree[idx]=nums[tl];
-		else{
-			int tm=tl+(tr-tl)/2;
-			TreeBuild(nums,idx*2,tl,tm);
-			TreeBuild(nums,idx*2+1,tm+1,tr);
-			tree[idx]=combine(tree[idx*2],tree[idx*2+1]);
-		}
-	}
     NumArray(vector<int>& nums) {
-        int len=nums.size();
-        siz=len;
-        tree.resize(len*4);
-        TreeBuild(nums,1,0,len-1);
+        n=nums.size();
+        for(ll i=0;i<n;i++)arr[i]=nums[i];
+        build();
     }
-    void TreeUpdate(int idx, int tl, int tr, int pos, int val){
-		if(tl==tr)tree[idx]=val;
-		else{
-			int tm=tl+(tr-tl)/2;
-			if(pos<=tm)TreeUpdate(idx*2,tl,tm,pos,val);
-			else TreeUpdate(idx*2+1,tm+1,tr,pos,val);
-			tree[idx]=combine(tree[idx*2],tree[idx*2+1]);
-		}
-	}
-    void update(int index, int val) {
-        TreeUpdate(1,0,siz-1,index,val);
-    }
-    int TreeQuery(int idx, int tl, int tr, int l, int r){
-		if(l>r)return Trush;
-		if(l==tl&&r==tr)return tree[idx];
-		int tm=tl+(tr-tl)/2;
-		return combine(TreeQuery(idx*2,tl,tm,l,min(r,tm)),TreeQuery(idx*2+1,tm+1,tr,max(l,tm+1),r));
-	}
+    
     int sumRange(int left, int right) {
-        return TreeQuery(1,0,siz-1,left,right);
+        cout<<left<<" "<<right<<endl;
+        int ans=query(left,right);
+        return ans;
     }
 };
 
 /**
- * Your NumArray object wiint be instantiated and cainted as such:
+ * Your NumArray object will be instantiated and called as such:
  * NumArray* obj = new NumArray(nums);
- * obj->update(index,val);
- * int param_2 = obj->sumRange(left,right);
+ * int param_1 = obj->sumRange(left,right);
  */
