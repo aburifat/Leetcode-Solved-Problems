5c5b051271cbcf3fb28c3e7efd71b2706005d7f1
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-01-10 09:35:23 +0000

    Sync LeetCode submission - Same Tree (cpp)

--- AC-Submissions/problems/same_tree/solution.cpp
+++ AC-Submissions/problems/same_tree/solution.cpp
@@ -1,15 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool ckTheTrees(TreeNode* p, TreeNode* q){
-        if(!p||!q)return p==q;
-        if(p->val!=q->val)return false;
-        bool ckLeft=ckTheTrees(p->left,q->left);
-        bool ckRight=ckTheTrees(p->right,q->right);
-        if(!ckLeft||!ckRight)return false;
-        return true;
-    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        bool ans=ckTheTrees(p,q);
+        if(p==nullptr||q==nullptr)return (p==q)?true:false;
+        bool ans=(p->val==q->val);
+        if(ans)ans&=(isSameTree(p->left,q->left)&isSameTree(p->right,q->right));
         return ans;
     }
 };
