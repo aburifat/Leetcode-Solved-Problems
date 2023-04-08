9256b92c0d6958f86cfd4005ff0e97c36ea1473e
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-04-08 10:45:09 +0000

    Sync LeetCode submission - Clone Graph (cpp)

--- AC-Submissions/problems/clone_graph/solution.cpp
+++ AC-Submissions/problems/clone_graph/solution.cpp
@@ -19,57 +19,24 @@ public:
 };
 */
 
-
-bool visited[110];
-
-void init(){
-    for(int i=0;i<110;i++){
-        visited[i]=false;
-    }
-}
-
 class Solution {
+    bool visited[110];
+    Node* new_add[110];
+    Node* clone(Node* node){
+        Node* new_node = new Node(node->val);
+        visited[node->val]=true;
+        new_add[node->val]=new_node;
+        for(int i=0;i<(int)node->neighbors.size();i++){
+            if(visited[node->neighbors[i]->val])new_node->neighbors.push_back(new_add[node->neighbors[i]->val]);
+            else new_node->neighbors.push_back(clone(node->neighbors[i]));
+        }
+        return new_node;
+    }
 public:
     Node* cloneGraph(Node* node) {
-        init();
-        if(node==nullptr)return nullptr;
-        vector<Node*>nodeList(110);
-        nodeList[node->val]=node;
-        visited[node->val]=true;
-        queue<Node*>q;
-        vector<int>nodeVal;
-        nodeVal.push_back(1);
-        q.push(node);
-        while(!q.empty()){
-            node=q.front();
-            q.pop();
-            for(int i=0;i<node->neighbors.size();i++){
-                if(!visited[node->neighbors[i]->val]){
-                    visited[node->neighbors[i]->val]=true;
-                    q.push(node->neighbors[i]);
-                    nodeList[node->neighbors[i]->val]=node->neighbors[i];
-                    
-                    nodeVal.push_back(node->neighbors[i]->val);
-                }
-            }
-        }
-        vector<Node*>newNodes;
-        for(int i=0;i<=110;i++){
-            Node* tmp=new Node();
-            newNodes.push_back(tmp);
-        }
-        sort(nodeVal.begin(),nodeVal.end());
-        Node* root=newNodes[1];
-        for(int i=0;i<nodeVal.size();i++){
-            newNodes[nodeVal[i]]->val=nodeVal[i];
-        }
-        for(int i=0;i<nodeVal.size();i++){
-            Node* tmp=nodeList[nodeVal[i]];
-            for(int j=0;j<tmp->neighbors.size();j++){
-                int val=tmp->neighbors[j]->val;
-                newNodes[nodeVal[i]]->neighbors.push_back(newNodes[val]);
-            }
-        }
+        for(int i=0;i<110;i++)visited[i]=false;
+        Node* root = nullptr;
+        if(node!=nullptr)root=clone(node);
         return root;
     }
 };
