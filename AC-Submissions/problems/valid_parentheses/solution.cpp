def5beb9292d94245d56c5369f2213af11ce018f
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-04-10 07:31:10 +0000

    Sync LeetCode submission - Valid Parentheses (cpp)

--- AC-Submissions/problems/valid_parentheses/solution.cpp
+++ AC-Submissions/problems/valid_parentheses/solution.cpp
@@ -1,33 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
+        int len = s.size();
         stack<char>st;
-        char openings[]={'(','{','['};
-        char closings[]={')','}',']'};
-        map<char,int>openMp,closeMp;
-        for(int i=0;i<3;i++)openMp[openings[i]]=i+1;
-        for(int i=0;i<3;i++)closeMp[closings[i]]=i+1;
-        int ck=0;
-        int len=s.size();
         for(int i=0;i<len;i++){
-            char tmp=s[i];
-            if(openMp[tmp])st.push(tmp);
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')st.push(s[i]);
             else{
-                int idx=closeMp[tmp]-1;
-                tmp=openings[idx];
-                if(st.empty()){
-                    ck=1;
-                    break;
+                if(st.empty())return false;
+                if(s[i]==')'){
+                    if(st.top()=='(')st.pop();
+                    else break;
                 }
-                if(tmp==st.top())st.pop();
+                else if(s[i]=='}'){
+                    if(st.top()=='{')st.pop();
+                    else break;
+                }
                 else{
-                    ck=1;
-                    break;
+                    if(st.top()=='[')st.pop();
+                    else break;
                 }
             }
         }
-        if(!st.empty())ck=1;
-        if(ck)return false;
-        else return true;
+        if(st.empty())return true;
+        return false;
     }
 };
