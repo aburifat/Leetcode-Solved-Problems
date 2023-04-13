8050114929f4fe7a1e221331507f8c85b462f4e4
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-04-13 19:47:57 +0000

    Sync LeetCode submission - Permutation Sequence (cpp)

--- AC-Submissions/problems/permutation_sequence/solution.cpp
+++ AC-Submissions/problems/permutation_sequence/solution.cpp
@@ -1,23 +1,38 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int>v;
+        k--;
         string s="";
-        for(int i=1;i<=n;i++)v.push_back(i);
-        int ct=1;
-        do{
-            if(ct==k){
-                for(int i=0;i<n;i++){
-                    s+=(v[i]+'0');
-                }
-                break;
-            }
-            ct++;
-        }while(next_permutation(v.begin(),v.end()));
-        if(s==""){
-            for(int i=0;i<n;i++){
-                s+=(v[i]+'0');
-            }
+
+        ordered_set oss;
+        for(int i=1;i<=n;i++)oss.insert(i);
+
+        // n!
+        int fact = 1;
+        for(int i=2;i<=n;i++){
+            fact*=i;
+        }
+
+        for(int i=n;i>0;i--){
+            fact/=i; // (n-1)!
+            int idx = (i==1)? 0 : (k/fact);
+            k %= fact;
+            int val=*oss.find_by_order(idx);
+            s+=to_string(val);
+            oss.erase(oss.find_by_order(idx));
         }
         return s;
     }
