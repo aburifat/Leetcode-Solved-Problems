5b14d9c4a7551d65162acc3a5761afe94fa3e84c
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-04-17 22:31:10 +0000

    [LeetCode Sync] Runtime - 123 ms (5.38%), Memory - 67.8 MB (99.97%)

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
-    int divide(vector<int>& nums, int l, int r){
-        if(l>r)return INT_MIN;
-        if(l==r)return nums[l];
-        int mid=l+(r-l)/2;
-        int left,right,sum;
-        left=right=sum=nums[mid];
-        for(int i=mid-1;i>=l;i--){
-            sum+=nums[i];
-            left=max(left,sum);
-        }
-        sum=nums[mid];
-        for(int i=mid+1;i<=r;i++){
-            sum+=nums[i];
-            right=max(right,sum);
-        }
-        int subSum=max(left+right-nums[mid],max(left,right));
-        return max(subSum,max(divide(nums,l,mid-1),divide(nums,mid+1,r)));
-    }
     int maxSubArray(vector<int>& nums) {
-        int len=nums.size();
-        int ans=divide(nums,0,len-1);
+        int ans = -100000;
+        int sum = 0;
+        for(int i=0;i<(int)nums.size();i++){
+            sum=max(sum+nums[i],nums[i]);
+            ans=max(ans,sum);
+        }
         return ans;
     }
 };
