ac92e31abdba85c0a087bf555e68d650b55f59a7
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-05-10 08:47:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6 MB (100.00%)

--- 0060-permutation-sequence/solution.cpp
+++ 0060-permutation-sequence/solution.cpp
@@ -1,38 +1,34 @@
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-
-using namespace __gnu_pbds;
-
-typedef tree<
-int,
-null_type,
-less<int>,
-rb_tree_tag,
-tree_order_statistics_node_update>
-ordered_set;
-
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        k--;
+        vector<bool>ck(n+1,false);
+        int p=1;
+        for(int i=2;i<n;i++)p*=i;
         string s="";
-
-        ordered_set oss;
-        for(int i=1;i<=n;i++)oss.insert(i);
-
-        // n!
-        int fact = 1;
-        for(int i=2;i<=n;i++){
-            fact*=i;
+        n--;
+        for(int j=n;j>=1;j--){
+            int idx=k/p;
+            if(k%p!=0)idx++;
+            int ct=0;
+            for(int i=1;i<=n+1;i++){
+                if(ck[i]==false){
+                    ct++;
+                    if(ct==idx){
+                        ck[i]=true;
+                        s+=('0'+i);
+                        break;
+                    }
+                }
+            }
+            k%=p;
+            if(k==0)k=p;
+            p/=j;
         }
-
-        for(int i=n;i>0;i--){
-            fact/=i; // (n-1)!
-            int idx = (i==1)? 0 : (k/fact);
-            k %= fact;
-            int val=*oss.find_by_order(idx);
-            s+=to_string(val);
-            oss.erase(oss.find_by_order(idx));
+        for(int i=1;i<=n+1;i++){
+            if(ck[i]==false){
+                s+=('0'+i);
+                break;
+            }
         }
         return s;
     }
