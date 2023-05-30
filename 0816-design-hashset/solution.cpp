859ed05f61a1b4a88351393e237b2da7ce1c34aa
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-05-30 23:36:43 +0000

    [LeetCode Sync] Runtime - 96 ms (13.92%), Memory - 43.7 MB (100.00%)

---
+++ 0816-design-hashset/solution.cpp
@@ -0,0 +1,27 @@
+class MyHashSet {
+public:
+    vector<bool>v;
+    MyHashSet() {
+        v.assign(1e6+10,false);
+    }
+    
+    void add(int key) {
+        v[key]=true;
+    }
+    
+    void remove(int key) {
+        v[key]=false;
+    }
+    
+    bool contains(int key) {
+        return v[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
