0679d4360ace2ffc27b2c44284fd79550ce735c8
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-06-02 20:30:17 +0000

    [LeetCode Sync] Runtime - 182 ms (22.52%), Memory - 14.5 MB (100.00%)

--- 2206-detonate-the-maximum-bombs/solution.cpp
+++ 2206-detonate-the-maximum-bombs/solution.cpp
@@ -1,59 +1,49 @@
-#include<bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-vector<vector<ll>>adj;
-vector<vector<int>>bomb;
-map<ll,bool>visited;
-
-ll bfs(ll s){
-    visited.clear();
-    ll ct=0;
-    queue<ll>q;
-    q.push(s);
-    while(!q.empty()){
-        s=q.front();
-        q.pop();
-        visited[s]=true;
-        ct++;
-        ll len=adj[s].size();
-        for(ll i=0;i<len;i++){
-            ll u=adj[s][i];
-            if(!visited[u]){
-                visited[u]=true;
-                q.push(u);
-            }
-        }
-    }
-    return ct;
-}
-
 class Solution {
 public:
-    int maximumDetonation(vector<vector<int>>& bombs) {
-        bomb=bombs;
-        ll n=bomb.size();
-        adj.assign(n+5,vector<ll>(0));
-        for(ll i=0;i<n;i++){
-            for(ll j=0;j<n;j++){
-                if(i==j)continue;
-                ll xx=abs(bomb[i][0]-bomb[j][0]);
-                xx*=xx;
-                ll yy=abs(bomb[i][1]-bomb[j][1]);
-                yy*=yy;
-                ll dis=xx+yy;
-                ll dd=bomb[i][2];
-                dd*=dd;
-                if(dis<=dd)adj[i].push_back(j);
-                dd=bomb[j][2];
-                dd*=dd;
-                if(dis<=dd)adj[j].push_back(i);
+    long long sq(long long num){
+        return num*num;
+    }
+    bool inRange(int i, int j,  vector<vector<int>>& bombs){
+        long long x1=bombs[i][0];
+        long long x2=bombs[j][0];
+        long long y1=bombs[i][1];
+        long long y2=bombs[j][1];
+        long long rad = bombs[i][2];
+        long long dis1 = sq(bombs[i][0]-bombs[j][0]) + sq(bombs[i][1]-bombs[j][1]);
+        long long dis2 = sq(rad);
+        if(dis1<=dis2)return true;
+        return false;
+    }
+    vector<vector<int>>adj;
+    vector<bool>visited;
+
+    int dfs(int idx, vector<vector<int>>& bombs){
+        visited[idx]=true;
+        int ct = 1;
+        for(int i=0;i<(int)adj[idx].size();i++){
+            int nxt=adj[idx][i];
+            if(!visited[nxt]){
+                ct+=dfs(nxt,bombs);
             }
         }
-        ll ans=0;
-        for(ll i=0;i<n;i++){
-            ll val=bfs(i);
-            ans=max(ans,val);
+        return ct;
+    }
+    
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int len = bombs.size();
+        adj.assign(len,vector<int>());
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len;j++){
+                if(i==j)continue;
+                if(inRange(i,j,bombs)){
+                    adj[i].push_back(j);
+                }
+            }
+        }
+        int ans=0;
+        for(int i=0;i<len;i++){
+            visited.assign(len,false);
+            ans=max(ans,dfs(i,bombs));
         }
         return ans;
     }
