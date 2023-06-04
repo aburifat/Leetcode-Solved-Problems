c7c1613156e38ae738774501d64c313d4ee8f942
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-06-04 15:40:49 +0000

    [LeetCode Sync] Runtime - 26 ms (5.00%), Memory - 14.1 MB (100.00%)

--- 0547-number-of-provinces/solution.cpp
+++ 0547-number-of-provinces/solution.cpp
@@ -1,38 +1,25 @@
 class Solution {
-    vector<vector<int>>adj;
-    vector<bool>visited;
 public:
-    void bfs(int s){
-        queue<int>q;
-        q.push(s);
-        visited[s]=true;
-        while(!q.empty()){
-            s=q.front();
-            q.pop();
-            for(int i=0;i<(int)adj[s].size();i++){
-                int u=adj[s][i];
-                if(!visited[u]){
-                    visited[u]=true;
-                    q.push(u);
-                }
-            }
-        }
-    }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected.size();
-        adj.assign(n+5,vector<int>());
-        visited.assign(n+5,false);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i==j)continue;
-                if(isConnected[i][j]==1)adj[i].push_back(j);
-            }
-        }
-        int ct=0;
+        int n = isConnected.size();
+        vector<bool>visited(n,false);
+        int ct = 0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 ct++;
-                bfs(i);
+                queue<int>q;
+                q.push(i);
+                visited[i]=true;
+                while(!q.empty()){
+                    int s = q.front();
+                    q.pop();
+                    for(int j=0;j<n;j++){
+                        if(isConnected[s][j]&&!visited[j]){
+                            visited[j]=true;
+                            q.push(j);
+                        }
+                    }
+                }
             }
         }
         return ct;
