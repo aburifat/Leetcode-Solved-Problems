b607eee9af45034362fc5715781f795af7eaf342
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-07-18 16:10:24 +0000

    [LeetCode Sync] Runtime - 18 ms (7.85%), Memory - 16.8 MB (99.93%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,10 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-            ans^=nums[i];
-        }
+        int ans = 0;
+        for(int i=0;i<(int)nums.size();i++)ans^=nums[i];
         return ans;
     }
 };
