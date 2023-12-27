02be96d4b8b66fe6bda02e49529ddd5b08830c6b
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2023-12-27 00:29:49 +0000

    [LeetCode Sync] Runtime - 138 ms (5.04%), Memory - 95.6 MB (100.00%)

--- 1700-minimum-time-to-make-rope-colorful/solution.cpp
+++ 1700-minimum-time-to-make-rope-colorful/solution.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int len =colors.size();
-        int cost=0;
-        for(int i=1;i<len;i++){
-            if(colors[i-1]==colors[i]){
-                cost+=min(neededTime[i-1],neededTime[i]);
-                neededTime[i]=max(neededTime[i-1],neededTime[i]);
+        int preTime = 0;
+        char preColor = '#';
+        int ans = 0;
+        for(int i=0;i<(int)colors.size();i++){
+            if(preColor == colors[i]){
+                ans += min(preTime,neededTime[i]);
+                preTime = max(preTime, neededTime[i]);
+            }else{
+                preTime = neededTime[i];
+                preColor = colors[i];
             }
         }
-        return cost;
+        return ans;
     }
 };
