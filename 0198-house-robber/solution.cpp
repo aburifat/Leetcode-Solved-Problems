8a5683f57d6069e1d686f43c31d02d3f4e8264c6
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-01-24 10:00:58 +0000

    [LeetCode Sync] Runtime - 4 ms (0.80%), Memory - 9.1 MB (100.00%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
-public:
     int dp[110];
-    int ctDp(vector<int>& nums, int idx){
+    int solve(int idx,vector<int>& nums){
         if(idx>=nums.size())return 0;
         if(dp[idx]!=-1)return dp[idx];
-        int val1=ctDp(nums,idx+1);
-        int val2=nums[idx]+ctDp(nums,idx+2);
-        dp[idx]=max(val1,val2);
-        return dp[idx];
+        return dp[idx] = max(solve(idx+1,nums),nums[idx]+solve(idx+2,nums));
     }
+public:
     int rob(vector<int>& nums) {
-        for(int i=0;i<110;i++)dp[i]=-1;
-        int ans=ctDp(nums,0);
-        if(nums.size()>1)ans=max(ans,ctDp(nums,1));
-        return ans;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,nums);
     }
 };
