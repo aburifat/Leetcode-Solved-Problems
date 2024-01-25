1bed7bbee8351258b9e107d7e81f44e819d24b17
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-01-25 02:56:14 +0000

    [LeetCode Sync] Runtime - 87 ms (5.09%), Memory - 25.8 MB (81.25%)

--- 1250-longest-common-subsequence/solution.cpp
+++ 1250-longest-common-subsequence/solution.cpp
@@ -1,23 +1,15 @@
-const int MAX_LEN=1000;
-    int mem[1000][1000];
-    int n, m;
-int lcs(int i, int j, string &s1, string &s2){
-    if(i==0||j==0)return 0;
-    if(mem[i][j]!=-1){
-        return mem[i][j];
-    }
-    int ans=0;
-    if(s1[i-1]==s2[j-1])ans=1+lcs(i-1,j-1,s1,s2);
-    else ans=max(lcs(i-1,j,s1,s2),lcs(i,j-1,s1,s2));
-    return mem[i][j]=ans;
-}
 class Solution {
+    vector<vector<int>>dp;
+    int solve(int i,int j, string& a, string& b){
+        if(i>=a.size()||j>=b.size())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        dp[i][j]=max(solve(i,j+1,a,b),solve(i+1,j,a,b));
+        if(a[i]==b[j])dp[i][j]=max(dp[i][j],1+solve(i+1,j+1,a,b));
+        return dp[i][j];
+    }
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        n=text1.size();
-        m=text2.size();
-        memset(mem, -1, sizeof mem);
-        int ans=lcs(n,m,text1,text2);
-        return ans;
+        dp.assign(text1.size(),vector(text2.size(),-1));
+        return solve(0,0,text1,text2);
     }
 };
