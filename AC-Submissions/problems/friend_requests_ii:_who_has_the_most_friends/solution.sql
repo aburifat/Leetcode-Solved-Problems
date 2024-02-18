9f0878239c1491683ea3aeaf4b62bf816cfb04a7
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-02-18 20:18:29 +0000

    Sync LeetCode submission - Friend Requests II: Who Has the Most Friends (mysql)

---
+++ AC-Submissions/problems/friend_requests_ii:_who_has_the_most_friends/solution.sql
@@ -0,0 +1,7 @@
+select user_id id, count(friend_id) num from
+((select requester_id user_id, accepter_id friend_id from RequestAccepted)
+union all
+(select accepter_id user_id, requester_id friend_id from RequestAccepted)) as friends
+group by id
+order by num desc
+limit 1
