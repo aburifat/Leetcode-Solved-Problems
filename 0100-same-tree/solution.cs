09bb920904f0e4fec0708633f4b71db8c2857de2
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-02-26 11:16:55 +0000

    [LeetCode Sync] Runtime - 59 ms (0.56%), Memory - 42.4 MB (100.00%)

---
+++ 0100-same-tree/solution.cs
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    bool checkSimilar(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if(p == null || q == null) return false;
+        if(p.val != q.val) return false;
+        return checkSimilar(p.left, q.left) && checkSimilar(p.right, q.right);
+    }
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        return checkSimilar(p,q);
+    }
+}
