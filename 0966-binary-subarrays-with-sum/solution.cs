1a05775025d6ebbd7690b02187ed07f1c9b8121c
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-03-14 17:57:39 +0000

    [LeetCode Sync] Runtime - 101 ms (26.55%), Memory - 50.3 MB (99.11%)

---
+++ 0966-binary-subarrays-with-sum/solution.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int NumSubarraysWithSum(int[] nums, int goal) {
+        int[] preSum = new int[nums.Length + 1];
+        preSum[0] = 1;
+        int sum = 0;
+        int ans = 0;
+        for(int i = 0; i < nums.Length; i++){
+            sum += nums[i];
+            if(sum >= goal){
+                int diff = sum - goal;
+                ans += preSum[diff];
+            }
+            preSum[sum]++;
+        }
+        return ans;
+    }
+}
