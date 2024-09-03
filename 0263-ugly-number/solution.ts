187da9d0d065a0ea6978bb2f8d37e0a4854020fb
Abu Rifat Muhammed Al Hasib <rifat.cse4.bu@gmail.com>
2024-09-03 20:30:12 +0000

    [LeetCode Sync] Runtime - 81 ms (2.78%), Memory - 53.5 MB (100.00%)

---
+++ 0263-ugly-number/solution.ts
@@ -0,0 +1,9 @@
+function isUgly(n: number): boolean {
+    if(n === 1) return true;
+    else if(n === 0) return false;
+    while(n % 2 === 0) n /= 2;
+    while(n % 3 === 0) n /= 3;
+    while(n % 5 === 0) n /= 5;
+    if(n === 1) return true;
+    return false;
+};
